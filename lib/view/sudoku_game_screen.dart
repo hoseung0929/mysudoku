114b8dca790e29592b94b374c67c9f028a2a0af4
hoseung0929 <hoseung0929@gmail.com>
2025-06-21 12:21:04 +0900

    feat: 숫자패드 UI 개선 - 아이폰 계산기 스타일 적용, 4x4 그리드 레이아웃, 버튼 디자인 변경

--- lib/view/sudoku_game_screen.dart
+++ lib/view/sudoku_game_screen.dart
@@ -200,16 +200,47 @@ class _SudokuGameScreenState extends State<SudokuGameScreen> {
         width: 95,
         height: 70,
         decoration: BoxDecoration(
-          color: const Color(0xFF1E1E1E),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.white.withOpacity(0.9),
+              Colors.white.withOpacity(0.7),
+              Colors.white.withOpacity(0.5),
+            ],
+          ),
           borderRadius: BorderRadius.circular(28),
+          border: Border.all(
+            color: Colors.white.withOpacity(0.3),
+            width: 1,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, 4),
+            ),
+            BoxShadow(
+              color: Colors.white.withOpacity(0.8),
+              blurRadius: 10,
+              offset: const Offset(0, -2),
+            ),
+          ],
         ),
         child: Center(
           child: Text(
             number.toString(),
-            style: const TextStyle(
-              color: Colors.white,
+            style: TextStyle(
+              color: Colors.grey[800],
               fontSize: 28,
-              fontWeight: FontWeight.w500,
+              fontWeight: FontWeight.w600,
+              shadows: [
+                Shadow(
+                  color: Colors.black.withOpacity(0.1),
+                  offset: const Offset(0, 1),
+                  blurRadius: 2,
+                ),
+              ],
             ),
           ),
         ),
@@ -235,15 +266,39 @@ class _SudokuGameScreenState extends State<SudokuGameScreen> {
         width: 70,
         height: 70,
         decoration: BoxDecoration(
-          color: Colors.grey[900],
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.white.withOpacity(0.9),
+              Colors.white.withOpacity(0.7),
+              Colors.white.withOpacity(0.5),
+            ],
+          ),
           borderRadius: BorderRadius.circular(35),
+          border: Border.all(
+            color: Colors.white.withOpacity(0.3),
+            width: 1,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, 4),
+            ),
+            BoxShadow(
+              color: Colors.white.withOpacity(0.8),
+              blurRadius: 10,
+              offset: const Offset(0, -2),
+            ),
+          ],
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(
               icons[menuNumber - 1],
-              color: Colors.white,
+              color: Colors.grey[800],
               size: 32,
             ),
             const SizedBox(height: 4),
